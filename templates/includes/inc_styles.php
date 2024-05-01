02392468d645755da468bb0ed702469fcef1d158
Meruzz <brandonalmachi123@gmail.com>
2024-05-01 16:25:33 -0500

    Plantilla de SB Admin 2 separada por bloques

---
+++ templates/includes/inc_styles.php
@@ -0,0 +1,26 @@
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+
+<!-- Todo plugin debe ir debajo de esta línea -->
+<!-- Toastr CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+
+<!-- WaitMe CSS -->
+<link rel="stylesheet" href="<?php echo PLUGINS.'waitMe/waitMe.min.css'; ?>">
+
+<!-- Lightbox -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/css/lightbox.min.css">
+
+<!-- Estilos SB Admin 2 -->
+<link href="<?php echo ASSETS.'vendor/fontawesome-free/css/all.min.css'; ?>" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,600,700,800,900" rel="stylesheet">
+
+<!-- Custom styles for this template-->
+<link href="<?php echo CSS.'sb-admin-2.min.css'; ?>" rel="stylesheet">
+
+<!-- Estilos registrados manualmente -->
+<?php echo load_styles(); ?>
+
+<!-- Estilos personalizados deben ir en main.css o abajo de esta línea -->
+<link rel="stylesheet" href="<?php echo CSS.'main.css?v='.get_version(); ?>">
