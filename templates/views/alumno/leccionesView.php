e4ed6013c6d9968949620db625de8f4c5559b9d2
Meruzz <brandonalmachi123@gmail.com>
2024-05-15 02:34:08 -0500

    Responsive de alumnos corregido

--- templates/views/alumno/leccionesView.php
+++ templates/views/alumno/leccionesView.php
@@ -3,65 +3,55 @@
 <!-- DataTales Example -->
 <div class="card shadow mb-4">
     <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">
-            Lista de lecciones disponibles
-        </h6>
+        <h6 class="m-0 font-weight-bold text-primary">Lista de lecciones disponibles</h6>
     </div>
     <div class="card-body">
         <?php if (!empty($d->lecciones->rows)) : ?>
-            <ul class="list-group">
-                <!-- Encabezados de columnas -->
-                <li class="list-group-item">
-                    <div class="row">
-                        <div class="col-1"></div> <!-- Para icono, si es necesario, o remover si no se usa -->
-                        <div class="col-3">Materia</div>
-                        <div class="col-2">Profesor</div>
-                        <div class="col-2">Titulo de la lección</div>
-                        <div class="col-2">Disponible el:</div>
-                        <div class="col-2 text-right">Tienes hasta el:</div>
-                    </div>
-                </li>
-                <!-- Detalles de cada lección -->
-                <?php foreach ($d->lecciones->rows as $l) : ?>
-                    <li class="list-group-item">
-                        <div class="row">
-                            <div class="col-1">
-                                <a href="<?php echo sprintf('alumno/leccion/%s', $l->id); ?>">
-                                    <img src="<?php echo get_image('play.png'); ?>" alt="<?php echo $l->titulo; ?>" class="img-fluid" style="width: 40px;">
-                                </a>
-                            </div>
-                            <div class="col-3">
-                                <span class="text-dark"><strong><?php echo add_ellipsis($l->materia, 50); ?></strong></span>
-                            </div>
-                            <div class="col-2">
-                                <span class="text-dark"><?php echo add_ellipsis($l->profesor, 50); ?></span>
-                            </div>
-                            <div class="col-2">
-                                <span class="text-dark"><?php echo add_ellipsis($l->titulo, 50); ?></span>
-                            </div>
-                            <div class="col-2">
-                                <span class="text-dark"><?php echo format_date($l->fecha_inicial); ?></span>
-                                <span class="badge badge-<?php echo (strtotime($l->fecha_inicial) - time()) < 0 ? 'success' : 'danger'; ?>">
-                                    <?php echo (strtotime($l->fecha_inicial) - time()) < 0 ? 'Ya disponible.' : 'No disponible aún.'; ?>
-                                </span>
-                            </div>
-                            <div class="col-2">
-                                <span class="text-dark"><?php echo format_date($l->fecha_disponible); ?></span>
-                                <span class="badge badge-<?php echo (strtotime($l->fecha_disponible) - time()) < 0 ? 'danger' : 'light'; ?>">
-                                    <?php echo (strtotime($l->fecha_disponible) - time()) < 0 ? 'Ya no esta disponible.' : format_tiempo_restante($l->fecha_disponible); ?>
-                                </span>
-                            </div>
-                        </div>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
-
-
-            <?php echo $d->lecciones->pagination; ?>
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th width="5%"></th> <!-- Para icono -->
+                            <th>Materia</th>
+                            <th>Profesor</th>
+                            <th>Título de la lección</th>
+                            <th>Disponible el:</th>
+                            <th>Tienes hasta el:</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($d->lecciones->rows as $l) : ?>
+                            <tr>
+                                <td>
+                                    <a href="<?php echo sprintf('alumno/leccion/%s', $l->id); ?>">
+                                        <img src="<?php echo get_image('play.png'); ?>" alt="Play" class="img-fluid" style="width: 24px; height: 24px;"> <!-- Asegúrate de que las dimensiones están especificadas -->
+                                    </a>
+                                </td>
+                                <td><strong><?php echo $l->materia; ?></strong></td>
+                                <td><?php echo $l->profesor; ?></td>
+                                <td><?php echo $l->titulo; ?></td>
+                                <td>
+                                    <span class="text-dark"><?php echo format_date($l->fecha_inicial); ?></span><br>
+                                    <span class="badge badge-<?php echo (strtotime($l->fecha_inicial) - time()) < 0 ? 'success' : 'danger'; ?>">
+                                        <?php echo (strtotime($l->fecha_inicial) - time()) < 0 ? 'Ya disponible.' : 'No disponible aún.'; ?>
+                                    </span>
+                                </td>
+                                <td>
+                                    <span class="text-dark"><?php echo format_date($l->fecha_disponible); ?></span><br>
+                                    <span class="badge badge-<?php echo (strtotime($l->fecha_disponible) - time()) < 0 ? 'danger' : 'info'; ?>">
+                                        <?php echo (strtotime($l->fecha_disponible) - time()) < 0 ? 'Ya no esta disponible.' : format_tiempo_restante($l->fecha_disponible); ?>
+                                    </span>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+                <?php echo $d->lecciones->pagination; ?>
+            </div>
         <?php else : ?>
             <div class="py-5 text-center">
                 <img src="<?php echo IMAGES . 'undraw_professor.svg'; ?>" alt="No hay registros" style="width: 150px;">
-                <p class="text-muted mt-3">No hay lecciones disponibles.</p>
+                <p class="text-muted">No hay lecciones disponibles.</p>
             </div>
         <?php endif; ?>
     </div>
